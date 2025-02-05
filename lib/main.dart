503385f52d0a01d269cd79af738c523fea92e5a8
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2025-02-05 11:19:59 +0300

    Modifying main file to test firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    Fluttertoast.showToast(
+      msg: "Firebase Connected Successfully",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.green,
+      textColor: Colors.white,
+    );
+  } catch (e) {
+    Fluttertoast.showToast(
+      msg: "Firebase Connection Failed: $e",
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+    );
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +42,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: HomePage(),
-      //home: RegisterPage(),
     );
   }
 }
